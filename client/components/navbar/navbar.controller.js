763b251a8f52c0faba7e493c283c3373ab4aabf4
Carmelo Scollo <carmelo.scollo@gmail.com>
2014-09-11 19:36:20 +0200

    made navbar's isActive function more flexible

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -19,24 +19,35 @@ angular.module('wessApp')
 
     $scope.isCollapsed = true;
 
-    $scope.isActive = function(route) {
-        /* this if statement checks whether the current route path includes 
-        '/main', '/weather' or '/userdata' in order to highlight the corrispondent tab 
-        in the primary navbar.
-        The statement could be optimized by searching through $scope.menu.item.link 
-        but the main url, i.e. '/' is an issue for the search because it is always found
-        */
-        if ($location.path().search('/main') !== -1) {
-            return route === "/"; // '/main' would not be correct because the item.link corresponding to item.name === 'Home' is '/'
-        }
-        else if ($location.path().search('/weather') !== -1) {
-            return route === "/weather";
-        }
-        else if ($location.path().search('/user_data') !== -1) {
-            return route === "/user_data";
+    //this method is used to apply the active class to our navigation elements defined in $scope.menu
+    $scope.isActive = function(route) {         
+        //check on which hierarchy level we currently are
+        if ($location.path().split("/").length > 2) {
+          /*in here we are at least 1 level below top-level
+            so we grab the top-level part of the current path with $location.path().split("/")[1]
+            and compare it to the route of the navigation element
+          */
+          if (route === "/") {
+            /*being here means we were called by our navigation element "Home" while on a page below top-level
+              the "child" pages of our top-level page home have "/main" as top-level part of their route
+              but our top-level page home uses "/" as route
+              so instead of comparing the top-level portion of the "child" page's path to the route we compare it to "/main"
+            */
+            return "/" + $location.path().split("/")[1] === "/main";            
+          }
+          else {
+            /*for our other top-level navigation elements 
+              we simply compare the top-level part of the path
+              to the top-level navgition element's route
+            */
+            return "/" + $location.path().split("/")[1] === route;            
+          }                    
         }
         else {
-            return route === $location.path();
+          /*in here we are on the top-level 
+            so we just compare the current path to the route
+          */
+          return route === $location.path();
         }
     };
   });
