8f2509c420c2f80e4799578af72a1a34597c0682
Carmelo Scollo <carmelo.scollo@gmail.com>
2014-09-28 13:29:12 +0200

    Implemented static use of hourlyAvgForDay API endpoint in client

--- client/app/weather/weather.controller.js
+++ client/app/weather/weather.controller.js
@@ -1,30 +1,38 @@
 'use strict';
 
 angular.module('wessApp')
-  .controller('WeatherCtrl', function ($scope) {
-    $scope.data = [
-      {x: 0, value: 4, otherValue: 14},
-      {x: 1, value: 8, otherValue: 1},
-      {x: 2, value: 15, otherValue: 11},
-      {x: 3, value: 16, otherValue: 147},
-      {x: 4, value: 23, otherValue: 87},
-      {x: 5, value: 42, otherValue: 45}
-    ];
+  .controller('WeatherCtrl', function ($scope, $http) {
+  
+    $scope.data;
+    $scope.isAPICallSuccessful;
+    $scope.sensorid = 197;
+    $scope.day = '2014-03-21';
     
     $scope.options = {
       axes: {
-        x: {key: 'x', labelFunction: function(value) {return value;}, type: 'linear', tooltipFormatter: function(x) {return x;}},
-        y: {type: 'linear', min: 0, max: 1},
-        y2: {type: 'linear', min: 0, max: 1}
+        x: {key: 'tick', type: 'date'},
+        y: {type: 'linear', min: -70, max: -50}
       },
       series: [
-        {y: 'value', color: 'steelblue', thickness: '2px', type: 'area', striped: true, label: 'Pouet'},
-        {y: 'otherValue', axis: 'y2', color: 'lightsteelblue', visible: false}
+        {y: 'value', color: 'steelblue', thickness: '2px', striped: true, label: 'Our data'}
       ],
       lineMode: 'linear',
       tension: 0.7,
       tooltipMode: 'dots',
       drawLegend: true,
       drawDots: true
-    };    
+    };
+    
+    $http.get('/api/data/hourlyAvgForDay', {params: {sensorid: $scope.sensorid, day: $scope.day}})
+    .success(function(result) {  
+      for (var i = 0; i < result.length; i++) {
+        result[i].tick = new Date(result[i].tick).getTime(); 
+      }
+      console.log(JSON.stringify(result));
+      $scope.data = result;
+      $scope.isAPICallSuccessful = true;})
+    .error(function(data, status, headers,config) {
+      $scope.isAPICallSuccessful = false;
+      console.log(data);
+    });
   });
