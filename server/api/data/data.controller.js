d89a999d2e238b09e70b5561059682f44a81b6b2
Carmelo Scollo <carmelo.scollo@gmail.com>
2014-09-28 12:41:32 +0200

    Implemented hourlyAvgForDay API endpoint

--- server/api/data/data.controller.js
+++ server/api/data/data.controller.js
@@ -15,6 +15,21 @@ exports.index = function(req, res) {
         err
       );
     }
-    
+  });
+};
+
+exports.hourlyAvgForDay = function(req, res) {
+  var sensid = req.query.sensorid;
+  var day = req.query.day;
+  query("SELECT date_trunc('hour', \"timestamp\") as tick, avg(value) as value FROM data_val WHERE \"timestamp\" BETWEEN $2::date AND $2::date + time '23:59:59' AND sensor = $1::int GROUP BY 1 ORDER BY 1 ASC", [sensid, day], function (err, rows, result) {
+    if(!err){
+      res.json(
+        rows
+      );
+    } else {
+      res.status(503).send(
+        err
+      );
+    }
   });
 };
--- server/api/data/index.js
+++ server/api/data/index.js
@@ -6,5 +6,6 @@ var controller = require('./data.controller');
 var router = express.Router();
 
 router.get('/', controller.index);
+router.get('/hourlyAvgForDay', controller.hourlyAvgForDay);
 
 module.exports = router;
