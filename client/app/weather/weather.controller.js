e86d1195e9303f4e9434b7af622fb0f287d522ae
Francesca d'Amico <missjulia83@gmail.com>
2014-10-24 14:31:10 +0200

    added the handling on the client: check whether the query response is empty or not

--- client/app/weather/weather.controller.js
+++ client/app/weather/weather.controller.js
@@ -31,14 +31,22 @@ angular.module('wessApp')
     
     $http.get('/api/data/hourlyAvgForDay', {params: {sensorid: sensorid, day: day }}) 
     .success(function(result) {
-      for (var i = 0; i < result.length; i++) {
-        result[i].tick = new Date(result[i].tick).getTime(); 
-      }
+        
+        //to check whether the query result is empty or not 
+        if (result.length === 0){
+            console.log ('the result of the query is empty');
+        }
+        else {
+            for (var i = 0; i < result.length; i++) {
+                result[i].tick = new Date(result[i].tick).getTime(); 
+            }
       
-      console.log(JSON.stringify(result));
+            console.log(JSON.stringify(result));
       
-      $scope.data = result;
-      $scope.isAPICallSuccessful = true;})
+            $scope.data = result;
+            $scope.isAPICallSuccessful = true;
+        }
+    })
     .error(function(data, status, headers,config) {
       $scope.isAPICallSuccessful = false;
       console.log(data);
