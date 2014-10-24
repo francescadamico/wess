1be6d738ecc6ba960750f54f9551ce0584bffda5
Francesca d'Amico <missjulia83@gmail.com>
2014-10-24 11:47:08 +0200

    changed the query in index to match the new db structure

--- server/api/data/data.controller.js
+++ server/api/data/data.controller.js
@@ -5,7 +5,7 @@ var query = require('pg-query');
 
 // Get number of measurement records
 exports.index = function(req, res) {
-  query.first('SELECT count(*) FROM data_val', function (err, result) {
+  query.first('SELECT count(*) FROM data_value', function (err, result) {
     if(!err){
       res.json(
         result
