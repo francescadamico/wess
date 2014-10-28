67b4efacf00b270d6bb498b414f345eb4aa52f3a
Francesca d'Amico <missjulia83@gmail.com>
2014-10-28 10:27:02 +0100

    added a 3-sites temperature plot in the weather page

--- client/app/weather/weather.controller.js
+++ client/app/weather/weather.controller.js
@@ -6,16 +6,23 @@ angular.module('wessApp')
     $scope.data;
     $scope.isAPICallSuccessful;
   
-    var sensorid = 210;
-    var day = new Date(Date.UTC(2013, 9, 9, 0, 0, 0)); //it creates a UTC date to be given to the server for the query
+    var station = [3,1,2];
+      var height = 250;
+      var sensortype = 7;
+      var measuredescr = 41;  
+
+    $scope.showDay = new Date(Date.UTC(2014, 3, 10, 0, 0, 0)); //it creates a UTC date to be given to the server for the query
     
+      var day = $scope.showDay;
     $scope.options = {
       axes: {
         x: {key: 'tick', type: 'date'},
-        y: {type: 'linear'}//, min: -70, max: -50}
+        y: {type: 'linear', min: 0, max: 20}
       },
       series: [
-        {y: 'value', color: 'steelblue', thickness: '2px', striped: true, label: 'Our data'}
+        {y: 'value1', color: 'steelblue', thickness: '2px', striped: true, label: 'Entringen'},
+          {y: 'value2', color: 'red', thickness: '2px', striped: true, label: 'Poltringen'},
+          {y: 'value3', color: 'green', thickness: '2px', striped: true, label: 'Tailfingen'}
       ],
       tooltip: {
         mode: "scrubber",
@@ -29,7 +36,7 @@ angular.module('wessApp')
       drawDots: true
     };
     
-    $http.get('/api/data/hourlyAvgForDay', {params: {sensorid: sensorid, day: day }}) 
+      $http.get('/api/data/hourlyAvgForDay3Sites', {params: {day: day, station: station, height: height, sensortype:sensortype, measuredescr:measuredescr}})
     .success(function(result) {
         
         //to check whether the query result is empty or not 
--- server/api/data/data.controller.js
+++ server/api/data/data.controller.js
@@ -34,3 +34,22 @@ exports.hourlyAvgForDay = function(req, res) {
     }
   });
 };
+
+exports.hourlyAvgForDay3Sites = function(req, res) {
+    var day = req.query.day;
+    var station = req.query.station;
+    var height = req.query.height;
+    var senstype = req.query.sensortype;
+    var measdescr = req.query.measuredescr;
+    
+    /* the query can be a sum of queries, use „UNION“ and for every column that  is missed in one query but needed in another query add in the SELECT part: „null as <missed_column_name>“
+    */
+    query("SELECT date_trunc('hour', data_value.timestamp) as tick, avg(data_value.value) as value1, null::numeric as value2, null::numeric as value3 FROM data_value, sensor WHERE data_value.sensor_id = sensor.sensor_id AND data_value.timestamp BETWEEN $1::timestamp AND $1::timestamp + time '23:59:59' AND sensor.station_id = $2::int AND sensor.height = $3::int AND sensor.sensor_type_id = $4::int AND data_value.measurement_description_id = $5::int GROUP BY 1 UNION SELECT date_trunc('hour', data_value.timestamp) as tick, null::numeric as value1, avg(data_value.value) as value2, null::numeric as value3 FROM data_value, sensor WHERE data_value.sensor_id = sensor.sensor_id AND data_value.timestamp BETWEEN $1::timestamp AND $1::timestamp + time '23:59:59' AND sensor.station_id = $6::int AND sensor.height = $3::int AND sensor.sensor_type_id = $4::int AND data_value.measurement_description_id = $5::int GROUP BY 1 UNION SELECT date_trunc('hour', data_value.timestamp) as tick, null::numeric as value1, null::numeric as value2, avg(data_value.value) as value3 FROM data_value, sensor WHERE data_value.sensor_id = sensor.sensor_id AND data_value.timestamp BETWEEN $1::timestamp AND $1::timestamp + time '23:59:59' AND sensor.station_id = $7::int AND sensor.height = $3::int AND sensor.sensor_type_id = $4::int AND data_value.measurement_description_id = $5::int GROUP BY 1 ORDER BY 1 ASC", [day, station[0], height, senstype, measdescr, station[1], station[2]], function (err, rows, result){
+         //checks errors in the connection to the db
+         if(!err){
+             res.json(rows);
+         } else {
+            res.status(503).send(err);
+         }
+     });
+ }; 
