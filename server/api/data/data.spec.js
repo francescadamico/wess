931004f4b4b7b40f89e303c018b65a7c02c85fdb
Carmelo Scollo <carmelo.scollo@gmail.com>
2014-11-02 19:52:17 +0100

    Added tests for data API endpoint

--- server/api/data/data.spec.js
+++ server/api/data/data.spec.js
@@ -4,17 +4,46 @@ var should = require('should');
 var app = require('../../app');
 var request = require('supertest');
 
-describe('GET /api/dataCount', function() {
+describe('GET /api/data', function() {
 
-  it('should respond with JSON array', function(done) {
+  it('should respond with count property', function(done) {
     request(app)
-      .get('/api/dataCount')
+      .get('/api/data')
       .expect(200)
       .expect('Content-Type', /json/)
       .end(function(err, res) {
         if (err) return done(err);
-        res.body.should.be.instanceof(Array);
+        res.body.should.have.property('count');
         done();
+      });
+  });
+});
+
+describe('GET /api/data/hourlyAvgForDay with valid inputs', function() {
+
+  it('should respond with data as JSON array', function(done) {
+    request(app)
+      .get('/api/data/hourlyAvgForDay?day=2014-03-22T00:00:00.000Z&sensorid=197')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end(function(err, res) {
+        if (err) return done(err);
+        res.body.should.be.instanceOf(Array);
+        done();
+      });
+  });
+});
+
+describe('GET /api/data/hourlyAvgForDay with invalid day and sensorid', function() {
+
+  it('should respond with HTTP 400', function(done) {
+    request(app)
+      .get('/api/data/hourlyAvgForDay?day=2014-03-32&sensorid=abc')
+      .expect(400)
+      .expect('Content-Type', /text/)
+      .end(function(err, res) {
+        if (err) return done(err);
+        done()
       });
   });
 });
