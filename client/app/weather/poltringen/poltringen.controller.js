becfd8003f97c04d899a27529dd7fca7ced77d61
Francesca d'Amico <missjulia83@gmail.com>
2014-11-11 16:14:03 +0100

    added the temperature plot in the Entringen, Poltringen and Tailfingen pages

--- client/app/weather/poltringen/poltringen.controller.js
+++ client/app/weather/poltringen/poltringen.controller.js
@@ -1,6 +1,59 @@
 'use strict';
 
 angular.module('wessApp')
-  .controller('PoltringenCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('PoltringenCtrl', function ($scope, $http) {
+    $scope.data;
+    $scope.isAPICallSuccessful;
+    $scope.resultIsEmpty;
+  
+      var station = 3; // Poltringen
+      var height = 250;
+      var measuredescr = 'avg';
+      var senstypedescr = 'temperature'; 
+      $scope.day = new Date(Date.UTC(2013, 9, 10)); //it creates a UTC date to be given to the server for the query
+      
+      $scope.monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+    
+    $scope.options = {
+      axes: {
+        x: {key: 'tick', type: 'date'},
+        y: {type: 'linear'}
+      },
+      series: [
+        {y: 'value', color: 'steelblue', thickness: '2px', striped: true, label: 'Poltringen'}
+      ],
+      tooltip: {
+        mode: "scrubber",
+        formatter: function (x, y, series) {
+          return moment(x).format("MMMM Do YYYY, h:mm:ss a") + ' : ' + y;
+        }
+      },
+      lineMode: 'linear',
+      tension: 0.7,
+      drawLegend: true,
+      drawDots: true
+    };
+    
+    $http.get('/api/data/hourlyAvgForDay', {params: {day: $scope.day, station: station, height: height, senstypedescr:senstypedescr, measuredescr:measuredescr}}) 
+    .success(function(result) {      
+      //to check whether the query result is empty or not 
+      if (result.length === 0){
+        $scope.resultIsEmpty = true;
+        $scope.isAPICallSuccessful = true;
+      }
+      else {
+        $scope.data = result.map(function(datum) {
+          return {
+            value: Number(datum.value),
+            tick: Date.parse(datum.tick)
+          };
+        });
+        $scope.resultIsEmpty = false;
+        $scope.isAPICallSuccessful = true;
+      }
+    })
+    .error(function(data, status, headers,config) {
+      $scope.isAPICallSuccessful = false;
+      console.log(data);
+    });
   });
