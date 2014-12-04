ed63ff163a2a14790691c1630d6b377e742bbeaa
Francesca d'Amico <missjulia83@gmail.com>
2014-12-04 13:57:40 +0100

    updated the Tailfingen page to match the new structure and added 5 different kind of plots

--- client/app/weather/tailfingen/tailfingen.controller.js
+++ client/app/weather/tailfingen/tailfingen.controller.js
@@ -2,58 +2,45 @@
 
 angular.module('wessApp')
   .controller('TailfingenCtrl', function ($http, $scope) {
-   $scope.data;
-    $scope.isAPICallSuccessful;
-    $scope.resultIsEmpty;
-  
-      var station = 2; // Tailfingen
-      var height = 250;
-      var measuredescr = 'avg';
-      var senstypedescr = 'temperature'; 
-      $scope.day = new Date(Date.UTC(2013, 9, 10)); //it creates a UTC date to be given to the server for the query
-      
+   $scope.day = new Date(Date.UTC(2014, 2, 10, 0, 0, 0)); //it creates a UTC date to be given to the server for the query
       $scope.monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
-    
-    $scope.options = {
-      axes: {
-        x: {key: 'tick', type: 'date'},
-        y: {type: 'linear'}
+      
+      $scope.station = 2;
+      $scope.plots = [
+      {
+          measurement_name:'Atmospheric Temperature',
+          senstypedescr:'temperature',
+          measdescr:'avg',
+          sensheight1: 150,
+          sensheight2: 250
       },
-      series: [
-        {y: 'value', color: 'red', thickness: '2px', striped: true, label: 'Tailfingen'}
-      ],
-      tooltip: {
-        mode: "scrubber",
-        formatter: function (x, y, series) {
-          return moment(x).format("MMMM Do YYYY, h:mm:ss a") + ' : ' + y;
-        }
+      {
+          measurement_name:'Atmospheric Humidity',
+          senstypedescr:'moisture',
+          measdescr:'avg',
+          sensheight1: 150,
+          sensheight2: 250
       },
-      lineMode: 'linear',
-      tension: 0.7,
-      drawLegend: true,
-      drawDots: true
-    };
-    
-    $http.get('/api/data/hourlyAvgForDay', {params: {day: $scope.day, station: station, height: height, senstypedescr:senstypedescr, measuredescr:measuredescr}}) 
-    .success(function(result) {      
-      //to check whether the query result is empty or not 
-      if (result.length === 0){
-        $scope.resultIsEmpty = true;
-        $scope.isAPICallSuccessful = true;
+      {
+          measurement_name:'Wind Speed',
+          senstypedescr:'wind speed',
+          measdescr:'avg',
+          sensheight1: 150,
+          sensheight2: 250
+      },
+      {
+          measurement_name:'Incoming Short-Wave Radiation',
+          senstypedescr:'pyranometer up',
+          measdescr:'avg',
+          sensheight1: 150,
+          sensheight2: 150
+      },
+      {
+          measurement_name:'Soil Temperature',
+          senstypedescr:'soil temperature',
+          measdescr:'sample',
+          sensheight1: 10,
+          sensheight2: 10
       }
-      else {
-        $scope.data = result.map(function(datum) {
-          return {
-            value: Number(datum.value),
-            tick: Date.parse(datum.tick)
-          };
-        });
-        $scope.resultIsEmpty = false;
-        $scope.isAPICallSuccessful = true;
-      }
-    })
-    .error(function(data, status, headers,config) {
-      $scope.isAPICallSuccessful = false;
-      console.log(data);
-    });
+    ];
   });
