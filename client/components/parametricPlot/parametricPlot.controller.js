af74e9fa326b40911c3b1fb3be6629fd048a76be
Francesca <missjulia83@gmail.com>
2015-05-26 13:12:04 +0200

    added the lastTs query to query the last timestamp in the database -> to display the last-day/last-month data in the weather page

--- client/components/parametricPlot/parametricPlot.controller.js
+++ client/components/parametricPlot/parametricPlot.controller.js
@@ -3,6 +3,9 @@
 angular.module('wessApp')
   .controller('ParametricplotCtrl', function ($scope, $http) {
       $scope.isAPICallSuccessful;
+    $scope.monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+    
+    
       /* loadPlot function:
        * it draws a plot with the result of the parametric query to the database
        * INPUTS: 
@@ -40,51 +43,71 @@ angular.module('wessApp')
               drawDots: true
           };
           
-          // the data to display are from the day before
-          if (day.getHours < 12)
-              var newDay = new Date(Date.UTC(day.getFullYear(), day.getMonth(), day.getDate()-1, 11, 52, 59));
-          else
-              var newDay = new Date(Date.UTC(day.getFullYear(), day.getMonth(), day.getDate(), 11, 52, 59));
-
-          var ii;
+          /* The $hhtp.get are performed one inside the other in order to get the queries answer in the right order, i.e. if each of them is inside a function that is called from the main code, they are called async, not allowing the dataQuery to use the result of the other two queries */
           
           /* get needed channel id numbers */
-              $http.get('/api/data/chnId',{params: {station:station, channel:channel, statistic:statistic}})
-                  .success(function(result) {
-                      //to check whether the query result is empty or not
-                      if (result.length === 0){
+          $http.get('/api/data/chnId',{params: {station:station, channel:channel, statistic:statistic}})
+              .success(function(result) {
+              // to check whether the query result is empty or not
+              if (result.length === 0){
+                  $scope.resultIsEmpty = true;
+                  $scope.isAPICallSuccessful = true;
+              }
+              else {
+                  var config;
+                  // all the found channels are put into the chn array and passed to the data query
+                  if (station === 'all') {
+                      
+                      $scope.chn_polt = [];
+                      $scope.chn_ent = [];
+                      $scope.chn_tail = [];
+                      for (var i_chn=0; i_chn < result.length; i_chn++) {
+                           if (result[i_chn].chn_id_polt !== null)
+                              $scope.chn_polt.push(parseInt(result[i_chn].chn_id_polt)); 
+                           if (result[i_chn].chn_id_ent !== null)
+                              $scope.chn_ent.push(parseInt(result[i_chn].chn_id_ent));
+                           if (result[i_chn].chn_id_tail !== null)
+                              $scope.chn_tail.push(parseInt(result[i_chn].chn_id_tail));
+                      };
+
+                      config = {params: {day: newDay, chn_polt:$scope.chn_polt, chn_ent:$scope.chn_ent, chn_tail:$scope.chn_tail, timeInterval:timeInterval, channel:channel, station:station}};
+                      // the parameter day: newDay will be added after the lastTs query
+                  }
+                  else { // only one station
+                              
+                      $scope.chn = [];
+                      for (i_chn=0; i_chn<result.length; i_chn++)
+                            $scope.chn.push(result[i_chn].chn_id);
+
+                      config = {params: {chn:$scope.chn, timeInterval:timeInterval, channel:channel, station:station}};
+                      // the parameter day: will be added after the lastTs query
+                  };
+                  
+                  /* find the last timestamp in the database -needed for the weather section that shows the last-day/last-month data  */
+                  $http.get('/api/data/lastTs')
+                      .success(function(resultTs) {
+                      // to check whether the query result is empty or not
+                      if (resultTs.length === 0) {
                           $scope.resultIsEmpty = true;
                           $scope.isAPICallSuccessful = true;
                       }
                       else {
-                          var config;
-                          // all the found channels are put into the chn array and passed to the data query
-                          if (station === 'all') {
-                              
-                              $scope.chn_polt = [];
-                              $scope.chn_ent = [];
-                              $scope.chn_tail = [];
-                              for (var i_chn=0; i_chn < result.length; i_chn++) {
-                                   if (result[i_chn].chn_id_polt !== null)
-                                      $scope.chn_polt.push(parseInt(result[i_chn].chn_id_polt)); 
-                                   if (result[i_chn].chn_id_ent !== null)
-                                      $scope.chn_ent.push(parseInt(result[i_chn].chn_id_ent));
-                                   if (result[i_chn].chn_id_tail !== null)
-                                      $scope.chn_tail.push(parseInt(result[i_chn].chn_id_tail));
-                              };
-                              
-                              config = {params: {day:newDay, chn_polt:$scope.chn_polt, chn_ent:$scope.chn_ent, chn_tail:$scope.chn_tail, timeInterval:timeInterval, channel:channel, station:station}};
-                              
-                          }
-                          else { // only one station
-                              
-                              $scope.chn = [];
-                              for (i_chn=0; i_chn<result.length; i_chn++)
-                                    $scope.chn.push(result[i_chn].chn_id);
-                              
-                              config = {params: {day:newDay, chn:$scope.chn, timeInterval:timeInterval, channel:channel, station:station}};
-                          };
+                          var lastTimestamp = new Date(resultTs[0].ts);
+                          var lastTimestamp_from = new Date();
+                          // sets the first date to be displayed to:
+                          //    - in case of 'One day' -> 1 day before the last timestamp found
+                          //    - in case of 'One month' -> 30 days before
+                          if (timeInterval === 'One day')
+                              lastTimestamp_from.setDate(lastTimestamp.getDate()-1);
+                          else
+                              lastTimestamp_from.setDate(lastTimestamp.getDate() -30);
+
+                          // string to display the date, e.g. "26 April 2015 -- 26 May 2015"
+                          $scope.dateString = lastTimestamp_from.getDate() + " " + $scope.monthNames[lastTimestamp_from.getMonth()] + " " + lastTimestamp_from.getFullYear() + " -- " + lastTimestamp.getDate() + " " + $scope.monthNames[lastTimestamp.getMonth()] + " " + lastTimestamp.getFullYear();
+
+                          config.params.day = lastTimestamp;
                           
+                          /* query the desired data */
                           $http.get('/api/data/dataQuery',config)
                               .success(function(response) {
                               if (response.length === 0){
@@ -101,7 +124,7 @@ angular.module('wessApp')
                                       });
                                       $scope.options.series[0].label = station;
                                   }
-                                  else {
+                                  else { // station == 'all'
                                       $scope.data = response.map(function(datum) {
                                           return {
                                               value1: Number(datum.val_polt),
@@ -115,19 +138,31 @@ angular.module('wessApp')
                                       $scope.options.series[2] = {y: 'value3', color: 'green', thickness: '2px', striped: true, label: 'Tailfingen'};
                                   };
                               };
+                              // ... continue dataQuery 
                               $scope.resultIsEmpty = false;
                               $scope.isAPICallSuccessful = true;
                           })
+                          // ... continue dataQuery
                               .error(function(data, status, headers,config) {
                                   console.log(data);
                           });
-                          $scope.resultIsEmpty = false; 
-                          $scope.isAPICallSuccessful = true;
-                      }
-              })
-                  .error(function(data, status, headers,config) {
-                        console.log(data);
-              });
-
+                      };
+                      // ... continue lastTs
+                      $scope.resultIsEmpty = false;
+                      $scope.isAPICallSuccessful = true;
+                  })
+                  // ... continue lastTs
+                      .error(function(data, status, headers) {
+                              console.log(data);
+                  });
+                  // ... continue chnId
+                  $scope.resultIsEmpty = false; 
+                  $scope.isAPICallSuccessful = true;
+              };
+          })
+          // ... continue chnId
+              .error(function(data, status, headers,config) {
+              console.log(data);
+          });
       };
   });
